c4371c9262df788a8d4407061db1ac4933f954fd
Rutuja1923 <rutujajirage19@gmail.com>
2025-05-24 12:05:59 +0530

    export all route modules from central index file for cleaner imports

---
+++ backend/src/routes/index.ts
@@ -0,0 +1,6 @@
+export { authRoutes } from "./auth-routes";
+export { userRoutes } from "./user-routes";
+export { postRoutes } from "./post-routes";
+export { commentRoutes } from "./comment-routes";
+export { likeRoutes } from "./like-routes";
+export { categoryRoutes } from "./category-routes";
